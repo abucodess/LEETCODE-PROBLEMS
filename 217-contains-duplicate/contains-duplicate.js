b3b904e311b726fe83de01bd2c74ee7fe6d70b19
aboobacker <abubackermax10@gmail.com>
2026-09-04 20:42:18 +0530

    Time: 29 ms (34.83%) | Memory: 79.2 MB (62.48%) - LeetSync

--- 217-contains-duplicate/contains-duplicate.js
+++ 217-contains-duplicate/contains-duplicate.js
@@ -3,15 +3,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    const set = new Set()
-for(let i=0;i<nums.length;i++){
-    if(set.has(nums[i])){
-        return true;
-    }
-    set.add(nums[i])
-}
-
-    return false
-
+    let set = new Set(nums)
+    return set.size!==nums.length
     
 };
