9566ba348e020033d308087e9c84ec3e4f3a6344
aboobacker <abubackermax10@gmail.com>
2026-09-05 16:14:20 +0530

    Time: 73 ms (8.43%) | Memory: 54.2 MB (78.05%) - LeetSync

--- 1-two-sum/two-sum.js
+++ 1-two-sum/two-sum.js
@@ -4,11 +4,11 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
-    for(let i =0;i<nums.length;i++){
-        for(let j=i+1;j<nums.length;j++){
-            if(nums[i]+nums[j]===target){
-                return [i,j]
-            }
+    for(let i = 0;i<nums.length;i++){
+        let num = target-nums[i]
+        index = nums.indexOf(num)
+        if(index!==-1 && index!=i){
+            return [i,index]
         }
     }
     
